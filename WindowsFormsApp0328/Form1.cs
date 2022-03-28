c202ce333c0109506c95b80bcab31852947f685d
user <user@DESKTOP-6JBEF5K>
2022-03-28 10:37:46 +0800

    textbox1

--- WindowsFormsApp0328/Form1.cs
+++ WindowsFormsApp0328/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace WindowsFormsApp0328
 {
@@ -15,6 +18,60 @@ namespace WindowsFormsApp0328
         public Form1()
         {
             InitializeComponent();
+        }
+        UdpClient U;
+        Thread th;
+        private void Listen()
+        {
+            int Port = int.Parse(textBox1.Text);
+            U = new UdpClient(Port);
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP);
+                textBox2.Text = Encoding.Default.GetString(B);
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            th = new Thread(Listen);
+            th.Start();
+            button1.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                th.Abort();
+                U.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string IP = textBoxIP.Text;
+            int Port = int.Parse(textBoxPort.Text);
+            byte[] B = Encoding.Default.GetBytes(textBoxText.Text);
+            UdpClient S = new UdpClient();
+            S.Send(B, B.Length, IP, Port);
+            S.Close();
         }
     }
 }
